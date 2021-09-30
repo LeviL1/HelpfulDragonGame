b441975a9525d8c1f70c6c45ec8896d7e673afc3
LeviL1 <78377282+LeviL1@users.noreply.github.com>
2021-09-30 18:21:16 -0500

    Adding in basic navmesh movement

--- Assets/Scripts/FollowPlayerScript.cs
+++ Assets/Scripts/FollowPlayerScript.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class FollowPlayerScript : MonoBehaviour
 {
+    
+    private Transform player; //player transform
+    private NavMeshAgent nav; //nav mesh of the enemy
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { //get nav mesh and player transfrom
+        nav = this.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate()
+    { //move to the player position on update
+       nav.destination = player.transform.position;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6.0f;
-    public float flightSpeed = 2.0f;
-    public float gravity = 20.0f;
+    public float speed = 6.0f; //speed of the player
+    public float flightSpeed = 2.0f; //speed player can ascend and descend 
+    public float gravity = 20.0f; //gravity 
 
-    private Vector3 moverDirection = Vector3.zero;
-    private CharacterController _cc;
-    // Start is called before the first frame update
+    private Vector3 moverDirection = Vector3.zero; //direction to move the player
+    private CharacterController _cc; // reference to character controller component on player
+    
     void Start()
-    {
+    {//get the character controller from the player
         _cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+    { //move the player in fixed update
         if (_cc.isGrounded != true)
         {
             moverDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
